63e1202c89dc1f4ed6a7f101e05865ceee652b88
nikitaforsh <forshnikita@gmail.com>
2026-09-16 21:18:25 +0300

    Created linear layer with shapeable parameters

--- gradforge/_tests/test_linear.py
+++ gradforge/_tests/test_linear.py
@@ -1,58 +1,47 @@
 from gradforge.core.tensor import Tensor
 from gradforge.optimizers.sgd import SGD
 from gradforge.autograd.standard_engine import StandardEngine
+from gradforge.backend.backend_manager import BackendManager
+from gradforge.training.layers.linear import Linear
 import tracemalloc
 
-tracemalloc.start()
-
-class Linear:
-    def __init__(self, x: Tensor, y: Tensor):
-        self.w = Tensor([], requires_grad=True)
-        self.x = x
-        self.y = y
-
-    @property
-    def params(self) -> list[Tensor]:
-        return [attr_val\
-                 for attr_val in self.__dict__.values()\
-                      if attr_val.requires_grad]
-
-    def predict(self, x: Tensor) -> Tensor:
-        return x @ self.w
+#tracemalloc.start()
     
-    def train(self, epochs):
-        self.w.data = self.x.backend.array_zeros((self.x.data.shape[1], y.data.shape[1]))
-        engine = StandardEngine()
-        optimizer = SGD(learning_rate=1e-3)
+def train(model, x, y, epochs):
+    engine = StandardEngine()
+    optimizer = SGD(learning_rate=1e-3)
+    for epoch in range(epochs):
+        y_pred  = model.predict(x)
+        error   = y - y_pred
+        loss    = error ** 2
 
-        for epoch in range(epochs):
-            y_pred  = self.predict(x)
-            error   = self.y - y_pred
-            loss    = error ** 2
-            engine.backward(loss)
-            optimizer.step(self.params)
-            optimizer.zero_grad(self.params)
+        optimizer.zero_grad(model.params)
+        engine.backward(loss)
+        print('eror grad: ', error.grad)
+        try:
+            optimizer.step(model.params)
+        except Exception as e:
+            print('Error: ', e)
 
-        print(self.x.data @ self.w.data)
+    print(x.data @ model.w.data)
 
 x       = Tensor([[4], [8], [9]])
 y       = Tensor([[8], [16], [18]])
-test    = Tensor([[9], [10]])
+test    = Tensor([[40], [60]])
 assert x.data.shape == y.data.shape
 
-before = tracemalloc.take_snapshot()
+#before = tracemalloc.take_snapshot()
 
-model = Linear(x, y)
-model.train(epochs=10000)
-
-after = tracemalloc.take_snapshot()
+model = Linear(1, 1)
+train(model, x, y, epochs=10000)
+# after = tracemalloc.take_snapshot()
 
 print(test.data @ model.w.data)
-print(model.w.data.shape)
+# print(model.w.data.shape)
 
-stats = after.compare_to(before, "lineno")
-for result in stats[:50]:
-    print(result)
+# stats = after.compare_to(before, "lineno")
+# for result in stats[:50]:
+#     print(result)
 
 # 0.757 seconds
 # Backend allocations needs to optimize
--- gradforge/training/layers/linear.py
+++ gradforge/training/layers/linear.py
@@ -1,20 +1,17 @@
 from .layer import Layer
 from ...core.tensor import Tensor
 from ...backend.backend_manager import BackendManager
-
 class Linear(Layer):
-    def __init__(self, x: Tensor, out_shape) -> None:
+    def __init__(self, in_shape, out_shape) -> None:
         super().__init__()      
-        self.out_shape = out_shape
         self._backend = BackendManager.get_backend()
-        self.x = x
-        self.w: Tensor = Tensor(self._backend.array_ones((x.data.shape()[1], out_shape)), requires_grad=True)
-
-    def predict(self) -> Tensor:
-        return self.x @ self.w
+        self.w: Tensor = Tensor(self._backend.array_zeros((in_shape, out_shape)), requires_grad=True)
+        
+    def predict(self, x: Tensor) -> Tensor:
+        return x @ self.w
 
     @property
     def params(self) -> list[Tensor]:
         return [attr_val\
                     for attr_val in self.__dict__.values()\
-                        if attr_val.requires_grad]
+                        if (type(attr_val) is Tensor and attr_val.requires_grad)]
